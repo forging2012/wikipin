70dbbc6568da0f7c26fb800586b1270222db7cdb
Hiromipaw <silvia@nopressure.co.uk>
2014-09-22 14:31:40 +0200

    updating routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get '/get_block', :to => 'blocks#get_block'
+      get 'blocks/get_block', :to => 'blocks#get_block'
+      get 'blocks/', :to => 'blocks#get_block'
       resources :blocks, :only => :show
-
       resources :pins, :only => [:index, :show]
     end
   end
